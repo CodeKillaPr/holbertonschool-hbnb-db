7a4c6850b8b9247b8af3478a0866fc933f6437f5
killa <killa@MBP-de-killa.home.lla.com>
2024-06-06 18:28:11 -0400

    fix somes code

--- model/city.py
+++ model/city.py
@@ -2,10 +2,10 @@ from model.BaseModel import BaseModel
 
 
 class City(BaseModel):
-    def __init__(self, country_id, name, **kwargs):
+    def __init__(self, name, country_code, **kwargs):
         super().__init__(**kwargs)
-        self.country_id = country_id
         self.name = name
+        self.country_code = country_code
 
     def __str__(self):
         return f"[City] ({self.id}) {self.to_dict()}"
