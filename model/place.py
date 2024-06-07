9c93bc2c1e8e7f1ef332eefd15b3db2000cb361a
killa <killa@MacBook-Pro-de-killa.local>
2024-06-07 13:00:24 -0400

    fix

--- model/place.py
+++ model/place.py
@@ -2,21 +2,21 @@ from model.BaseModel import BaseModel
 
 
 class Place(BaseModel):
-    def __init__(self, user_id, name, city_id, description="", number_rooms=0, number_bathrooms=0,
-                 max_guest=0, price_by_night=0, latitude=0.0, longitude=0.0, **kwargs):
+    def __init__(self, name, description, address, city_id, latitude, longitude, host_id,
+                 number_of_rooms, number_of_bathrooms, price_per_night, max_guests, amenity_ids=[], **kwargs):
         super().__init__(**kwargs)
-        self.user_id = user_id
         self.name = name
-        self.city_id = city_id
         self.description = description
-        self.number_rooms = number_rooms
-        self.number_bathrooms = number_bathrooms
-        self.max_guest = max_guest
-        self.price_by_night = price_by_night
+        self.address = address
+        self.city_id = city_id
         self.latitude = latitude
         self.longitude = longitude
-        self.amenities = []
-        self.reviews = []
+        self.host_id = host_id
+        self.number_of_rooms = number_of_rooms
+        self.number_of_bathrooms = number_of_bathrooms
+        self.price_per_night = price_per_night
+        self.max_guests = max_guests
+        self.amenity_ids = amenity_ids
 
     def __str__(self):
         return f"[Place] ({self.id}) {self.to_dict()}"
--- model/review.py
+++ model/review.py
@@ -2,11 +2,12 @@ from model.BaseModel import BaseModel
 
 
 class Review(BaseModel):
-    def __init__(self, place_id, user_id, text, **kwargs):
+    def __init__(self, place_id, user_id, rating, comment, **kwargs):
         super().__init__(**kwargs)
         self.place_id = place_id
         self.user_id = user_id
-        self.text = text
+        self.rating = rating
+        self.comment = comment
 
     def __str__(self):
         return f"[Review] ({self.id}) {self.to_dict()}"
